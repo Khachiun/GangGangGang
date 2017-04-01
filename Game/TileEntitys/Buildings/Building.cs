69dc4f211dbe2668d32f6565c5aaea61bdbc1490
JessyCzi <Jessy.Brodin@gmail.com>
2017-04-01 15:45:51 -0700

    makeing bulidings generic 0

--- Game/TileEntitys/Buildings/Building.cs
+++ Game/TileEntitys/Buildings/Building.cs
@@ -10,19 +10,22 @@ using Czaplicki.SFMLE;
 
 namespace GangGang
 {
-
- 
-    class Building : TileEntity
+    class House
     {
-        static Building()
+        static House()
         {
             RectangleShape shape = new RectangleShape(new Square() * Hexagon.HEX_SIZE * 2, new Texture("Content/Assets/Textures/Building.png"));
             DrawComponent.Regiser("Buliding", shape);
         }
+    }
+
+    class Building : TileEntity
+    {
+       
 
         public Building(int x, int y, Player owner) : base(x, y, new CircleCollition(Hexagon.HEX_R), owner)
         {
-            SpawnTileEntityComponent spawn = new SpawnTileEntityComponent("Spawn",10,3, (X, Y) => new Worker(X, Y, Owner));
+            SpawnTileEntityComponent spawn = new SpawnTileEntityComponent("Spawn", 10, 3, (X, Y) => new Worker(X, Y, Owner));
             spawn.Enable = false;
             Adopt(spawn);
             DrawComponent draw = new DrawComponent("Buliding", Layer.UNIT_BASE);
